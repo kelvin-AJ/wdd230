5444c23969afc21e89e876822e8516a08dfac8a0
kelvin-AJ <aji22001@byui.edu>
2022-06-29 16:25:38 +0100

    toggle view

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,19 +1,40 @@
 const directoryUrl = "https://kelvin-aj.github.io/wdd230/chamber/json/data.json";
 const directoryCont = document.querySelector(".directory-container");
+const listBtn = document.querySelector(".list-view-icon");
+const gridBtn = document.querySelector(".grid-view-icon");
+let company ;
+let companyInfo ;
+
+
+gridBtn.addEventListener("click", () => {
+    gridBtn.classList.add("active-switch");
+    listBtn.classList.remove("active-switch");
+    company.forEach(company=> company.classList.replace("list-company","grid-company"));
+
+    companyInfo.forEach(info => info.classList.replace("list-info", "grid-info"));
+    directoryCont.classList.replace("flex-directory", "grid-directory");
+});
+listBtn.addEventListener("click", () => {
+    listBtn.classList.add("active-switch");
+    gridBtn.classList.remove("active-switch");
+    company.forEach(company=> company.classList.replace("grid-company","list-company"));
+
+    companyInfo.forEach(info => info.classList.replace("grid-info", "list-info"));
+    directoryCont.classList.replace("grid-directory", "flex-directory");
+});
 
 async function getandshowDirectory() {
     const response = await fetch(directoryUrl);
     const jsonObject = await response.json();
     const directory = jsonObject.directory;
-    console.log(directory);
 
     directoryCont.innerHTML = "";
     directory.forEach((company) => {
         directoryCont.insertAdjacentHTML("beforeend", 
         `
-                    <div class="company abel-text">
+                    <div id="company" class="company grid-company abel-text">
                         <img class="company-img" src="${company.logoURL}" loading="lazy" alt="logo of ${company.name}">
-                        <div class="company-info">
+                        <div class="company-info grid-info">
                             <h2 class="mono-text">${company.name} <img src="images/star_FILL0_wght400_GRAD0_opsz24.svg" alt="${company.membership} tag" class="${company.membership}"></h2>
                             <ul>
                             <li>${company.email}</li>
@@ -24,6 +45,14 @@ async function getandshowDirectory() {
                             <p class="transparent">${company.description}</p>
                         </div>
                     </div>`)
+
+                    
     });
+    company = document.querySelectorAll("#company");
+    companyInfo = document.querySelectorAll(".company-info");
+
+
+
+    
 };
 getandshowDirectory();
